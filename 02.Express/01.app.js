76151e1fb7a7ec12f7805cc8a7e042a31f085eaf
jaykim0929 <killguga@naver.com>
2020-10-05 12:58:34 +0900

    express

---
+++ 02.Express/01.app.js
@@ -0,0 +1,27 @@
+const express = require('exepress');
+const utill = require('util');
+
+const app = express();
+
+app.get('/', function(req, res){
+    let html = `
+    <!DOCTYPE html>
+    <html lang="ko">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Express</title>
+    </head>
+    <body>
+        <h1>Welcome to Express world</h1>
+    </body>
+    </html>
+    `;
+    res.send(html);
+});
+//app.get();
+//app.post();
+
+app.listen(4000, () => {
+    console.log('Server Running at http://127.0.0.1:4000');
+});
